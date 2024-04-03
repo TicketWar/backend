6144dab6f3b4dcade9cf7e7a9207db8c127ac44f
kimminkyeu <minkyeki@student.42seoul.kr>
2024-04-03 18:00:27 +0900

    Project Initial commit

---
+++ src/main/java/com/ticketwar/ticketwar/TicketwarApplication.java
@@ -0,0 +1,13 @@
+package com.ticketwar.ticketwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketwarApplication.class, args);
+	}
+
+}
