ae23441eb9941635c9b972c60c1cd6caad7819f2
jun <yoseb95@gmail.com>
2024-04-08 11:00:54 +0900

    feat: Performance entity column 제약사안 추가 #4 생성일자 및 수정일자 어노테이션 추가 name column length 100 으로 제한 nullable option 추가

--- src/main/java/com/ticketwar/ticketwar/entity/Performance.java
+++ src/main/java/com/ticketwar/ticketwar/entity/Performance.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -17,28 +19,30 @@ public class Performance {
     // 공연 id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false)
+    @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
     //공연이름
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, length = 100)
     private String name;
 
     //티켓팅 시작
-    @Column(name = "start_at")
+    @Column(name = "start_at", nullable = false)
     private LocalDateTime start_at;
 
     //티켓팅 종료
-    @Column(name = "end_at")
+    @Column(name = "end_at", nullable = false)
     private LocalDateTime end_at;
 
     //생성일자
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime created_at;
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime created_at = LocalDateTime.now();
 
     //수정일자
-    @Column(name = "updated_at")
-    private LocalDateTime updated_at;
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updated_at = LocalDateTime.now();
 
     @Builder
     public Performance(String name, LocalDateTime start_at, LocalDateTime end_at) {
@@ -46,5 +50,4 @@ public class Performance {
         this.start_at = start_at;
         this.end_at = end_at;
     }
-
 }
