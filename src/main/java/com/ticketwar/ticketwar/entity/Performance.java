977a2e1316859acb436af5087397a61cf2a079e6
jun <yoseb95@gmail.com>
2024-04-08 10:41:40 +0900

    feat: Performance entity 초안 작성 #4 빌더패턴생성자 작성 생성일자 및 수정일자 로직 추가필요 각 column 제약조건 추가 필요

---
+++ src/main/java/com/ticketwar/ticketwar/entity/Performance.java
@@ -0,0 +1,48 @@
+package com.ticketwar.ticketwar.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name = "PERFORMANCE")
+public class Performance {
+
+    // 공연 id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    //공연이름
+    @Column(name = "name")
+    private String name;
+
+    //티켓팅 시작
+    @Column(name = "start_at")
+    private LocalDateTime start_at;
+
+    //티켓팅 종료
+    @Column(name = "end_at")
+    private LocalDateTime end_at;
+
+    //생성일자
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime created_at;
+
+    //수정일자
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+
+    @Builder
+    public Performance(String name, LocalDateTime start_at, LocalDateTime end_at) {
+        this.name = name;
+        this.start_at = start_at;
+        this.end_at = end_at;
+    }
+
+}
