08a9de8346e21d66ff0eabab38960b4c36c8b9dc
jun <yoseb95@gmail.com>
2024-04-11 18:08:10 +0900

    feat: PerformanceNotFoundException.java 개발 #7 repository.findById() 메소드의 리턴이 null일 경우 예외처리를 위한 개발

---
+++ src/main/java/com/ticketwar/ticketwar/performance/exception/PerformanceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ticketwar.ticketwar.performance.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class PerformanceNotFoundException extends RuntimeException {
+    public PerformanceNotFoundException(String message) {
+        super(message);
+    }
+}
