6145f80c794afc9eaa17e5e0a62cee72bc84eef1
jun <yoseb95@gmail.com>
2024-04-11 20:25:31 +0900

    fix: Exception ResponseStatus 누락 수정 CustomerNotFoundException, SeatNotFoundException @ResponseStatus() 누락 수정

--- src/main/java/com/ticketwar/ticketwar/seat/exception/SeatNotFoundException.java
+++ src/main/java/com/ticketwar/ticketwar/seat/exception/SeatNotFoundException.java
@@ -1,5 +1,9 @@
 package com.ticketwar.ticketwar.seat.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class SeatNotFoundException extends RuntimeException {
   public SeatNotFoundException(String msg) {
     super(msg);
