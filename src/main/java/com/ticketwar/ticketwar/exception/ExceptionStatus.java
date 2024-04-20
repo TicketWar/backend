2cb6b2e496b0f8d557cc5d1354657a5458eaa966
Photogrammer <khsj15@khu.ac.kr>
2024-04-20 22:38:39 +0900

    feat: 필요한 ExceptionStatus 추가

--- src/main/java/com/ticketwar/ticketwar/exception/ExceptionStatus.java
+++ src/main/java/com/ticketwar/ticketwar/exception/ExceptionStatus.java
@@ -5,7 +5,14 @@ import org.springframework.http.HttpStatus;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ExceptionStatus {
+  // user
+  // Not found
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+  // Bad Request
+  DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임입니다."),
+  DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "중복된 이메일입니다."),
+  UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "허가되지 않은 유저입니다."),
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류입니다."),
   ;
 
   private final int code;
